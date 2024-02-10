7acc6de27f5376a64985377b1325a9623d375b3c
Suman Pankhaniya <117751876+SHP05@users.noreply.github.com>
2024-02-10 19:45:03 +0530

    Update products.js

--- controllers/products.js
+++ controllers/products.js
@@ -9,13 +9,10 @@ const getAllProducts = async(req,res) =>{
         queryObject.company = company;
     }
 
-    //M. IMP
-    //name filter - if we write any name similar data will show which name is same using regex
     if(name){
         queryObject.name = { $regex: name , $options: "i" } // i represent incase sensitive 
     }
     
-    //same as above apply in feature
     if(feature)
     {
         queryObject.feature = feature;
@@ -23,46 +20,29 @@ const getAllProducts = async(req,res) =>{
 
     let apidata = Product.find(queryObject);
     
-    //sort
     if(sort){
         let sortfix = sort.split(",").join(" ");
         apidata = apidata.sort(sortfix);
     }
-
-
-    //select field
     if(select){
         let selectfix = select.split(",").join(" ");
         apidata = apidata.select(selectfix);
     }
-
-
-    //Pagination
     let page = Number(req.query.page) || 1;
     let limit = Number(req.query.limit) || 3;
 
     let skip = (page-1)*limit;
 
     apidata = apidata.skip(skip).limit(limit);
-    //formmula
-    //page = 2;
-    //limit = 3;
-    //skip = (2-1)*3 = 3
-
-
+   
     console.log(queryObject);
 
     //by using this if we search anything after company it must return data insted empty array
     const mydata =  await apidata
 
-
-    // const mydata =  await Product.find(req.query)
-    // const mydata =  await Product.find({})
     res.status(200).json({mydata, nbHits: mydata.length});
-    // res.status(200).json({msg : "I am getAllProducts "});
 }
 
-//create product
 const createProduct = async (req,res,next) =>{
     console.log(req.body.name);
     await Product.create({
@@ -88,7 +68,6 @@ const updateProduct = async (req,res,next) => {
     .then(result => res.json({message:"Product Data is updated"}))
     .catch(err => res.json('Product is not update',err));
 }
-
 
 const getAllProductsTesting = async(req,res) =>{
     const mydata =  await Product.find(req.query)
